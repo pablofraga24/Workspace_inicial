5b95a17faac3e0a4cde53630892fb0ceb4c43b7a
Juan Pablo Fraga Terra <juanpablofragaterra@gmail.com>
2020-08-17 00:02:58 -0300

    Workspace_inicial_V2

--- js/login.js
+++ js/login.js
@@ -1,6 +1,24 @@
-//Función que se ejecuta una vez que se haya lanzado el evento de
-//que el documento se encuentra cargado, es decir, se encuentran todos los
-//elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
+function validate() {
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    
+    if ((username == null || username.trim() === "") && (password == null || password.trim() === "")){
+        location.href="index.html"
+        alert("Por favor ingresa el usuario y la contraseña");
+        return false;
+    }
+    
+    if (username == null || username.trim() === "") {
+        location.href="index.html"
+        alert("Por favor ingresa el usuario");
+        return false;
+    }
+    if (password == null || password.trim() === "") {
+        location.href="index.html"
+        alert("Por favor ingresa la contraseña");
+        return false;
+    }
+    location.href="inicio.html"
+    alert('Te has logueado correctamente');
+}
 
-});
